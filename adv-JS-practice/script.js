04bde9e1e052aa111368b32cba74d11ac4906cbe
Brooke Zwayer <54447648+bmzwayer@users.noreply.github.com>
2020-05-18 09:23:27 -0500

    adv js practice problems-- solved

---
+++ adv-JS-practice/script.js
@@ -0,0 +1,431 @@
+// SCOPE
+
+// For all of these, what is the value of a when the function gets called with the alert()
+// #1
+function q1() {
+    var a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+    alert(a); //3
+}
+
+//#2
+// you must first run q2() in your console to add the new value. then run q22()
+var a = 0;
+function q2() {
+    a = 5;
+}
+
+function q22() {
+    alert(a); //5
+}
+
+// run in the console:
+// q22() -- 0
+// q2() -- undefined
+// q22() -- 5
+
+//#3
+function q3() {
+    window.a = "hello";
+}
+
+// you must first run q3() in your console to add the a property to the window. then run q32()
+function q32() {
+    alert(a); //"hello"
+}
+
+//#4
+var a = 1;
+function q4() {
+    var a = "test";
+    alert(a); //"test"
+}
+
+//#5 with var keyword, if statements do not create a new scope
+var a = 2;
+if (true) {
+    var a = 5;
+    alert(a); // 5
+}
+alert(a); // 5
+
+//CONTROL FLOW
+
+//#1 change this function into a ternary and assign it to variable called experiencePoints
+function experiencePoints() {
+    if (winBattle()) {
+        return 10;
+    } else {
+        return 1;
+    }
+}
+
+var experiencePoints = winBattle() ? 10 : 1;
+
+//Using this function, answer the questions below:
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            break;
+            whatHappens = "you encounter a monster";
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+            break;
+        case "right":
+            return whatHappens = "you found a river";
+            break;
+        case "left":
+            break;
+            return whatHappens = "you run into a troll";
+            break;
+        default:
+            whatHappens = "please enter a valid direction";
+    }
+    return whatHappens;
+}
+
+//#2 return value when moveCommand("forward");
+undefined
+
+//#3 return value when moveCommand("back");
+"you arrived home"
+
+//#4 return value when moveCommand("right");
+"you found a river"
+
+//#5 return value when moveCommand("left");
+undefined
+
+//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
+
+//ES5 + ES6
+
+// change everything below to the newer Javascript!
+
+// let + const
+let a = 'test';
+const b = true;
+const c = 789;
+a = 'test2';
+
+
+// Destructuring
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+const { firstName, lastName, age, eyeColor } = person;
+
+
+// Object properties
+const a = 'test';
+const b = true;
+const c = 789;
+
+const okObj = {a, b, c};
+
+
+// Template strings
+const message = `Hello ${firstName} have I met you before? I think we met in ${city}. last summer no???`;
+
+
+// default arguments
+// default age to 10;
+const isValidAge = (age = 10) => age;
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+const sym = Symbol('This is my first Symbol');
+
+// Arrow functions
+const whereAmI = (username, location) => {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+
+//ADV FUNCTIONS
+
+//Solve these problems:
+
+//#1 Create a one line function that adds adds two parameters
+const sum = (a, b) => a + b
+
+//Closure: What does the last line return?
+const addTo = x => y => x + y
+var addToTen = addTo(10)
+addToTen(3) // returns 13
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+curriedSum(30)(1) // 31
+
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+const add5 = curriedSum(5)
+add5(12) // 17
+
+//Composing: What does the last line return?
+const compose = (f, g) => (a) => f(g(a));
+const add1 = (num) => num + 1;
+const add5 = (num) => num + 5;
+compose(add1, add5)(10) // 16
+
+//What are the two elements of a pure function?
+// 1. Deterministic --> always produces the same results given the same inputs
+// 2. No Side Effects -->  It does not depend on any state, or data, change during a program’s execution. It must only depend on its input elements.
+
+//ADV OBJECTS
+
+//Evaluate these:
+//#1
+[2] === [2] // false
+// {} === {} // false
+
+//#2 what is the value of property a for each object.
+const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; // 5
+object1.a = 4;
+
+
+//#3 create two classes: an Animal class and a Mamal class. 
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal {
+	constructor(name, type, color) {
+		this.name = name;
+		this.color = color;
+		this.type = type;
+	}
+}
+
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+	}
+}
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
+
+//ES7
+
+// Solve the below problems:
+
+// #1) Check if this array includes the name "John".
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.includes('John') // false
+
+// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
+// name or names in an array.
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.filter(name => name.includes('John')) // ['Johnathan']
+
+// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+const power100 = (num) => num**100;
+
+// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// Research for yourself why you get this result.
+
+power100(10000) // Infinity
+
+//ES8
+
+// Solve the below problems:
+
+// #1) Line up the Turtle and the Rabbit at the start line:
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+
+// it should look like this:
+'     ||<- Start line'
+'       🐢'
+'       🐇'
+
+// when you do:
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+'     ||<- Start line'
+'🐢======='
+'       🐇'
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ')
+
+//ES10
+
+// Solve the below questions:
+
+// #1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
+const array = [[1],[2],[3],[[[4]]],[[[5]]]]
+//Solution:
+console.log(array.flat(2))
+
+
+// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+//Solution:
+console.log(greeting.flatMap(x => x.join(' ')))
+
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+//Solution
+console.log(greeting.flatMap(x => x.join(' ')).join(' '))
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+//Solution
+console.log(trapped.flat(Infinity))
+// Infintiy is actually a LARGE number in JavaScipt. It represents the maximum amount of memory that we can hold for a number! Learn more here: https://riptutorial.com/javascript/example/2337/infinity-and--infinity
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+//Solution:
+console.log(userEmail3.trimEnd().trimStart())
+
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+//Solution
+const usersArray = Object.entries(users)
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+//Solution
+updatedUsersArray = usersArray.map((user) => [user[0], user[1] * 2])
+
+//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+//Solution
+const updatedUsers = Object.fromEntries(updatedUsersArray)
+console.log(updatedUsers)
+
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+}
+
+//1
+for (let i = 0; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+
+//2
+basket.forEach(item => {
+  console.log(item);
+})
+
+for (item in detailedBasket) {
+  console.log(item);
+}
+
+for (item of basket) {
+  console.log(item);
+}
+
+//ADV LOOPS
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+function biggestNumberInArray(arr) {
+  let highest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (highest < arr[i]) {
+      highest = arr[i];
+    }
+  }
+  return highest
+}
+
+function biggestNumberInArray2(arr) {
+  let highest = 0;
+  arr.forEach(item => {
+    if (highest < item) {
+      highest = item;
+    }
+  })
+  return highest;
+}
+
+function biggestNumberInArray3(arr) {
+  let highest = 0;
+  for (item of arr) {
+    if (highest < item) {
+      highest = item;
+    }
+  }
+  return highest;
+}
+
+
+biggestNumberInArray3(array3)
+
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+}
+
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    if (item === lookingFor) {
+      return `${lookingFor} is in your basket`
+    }
+  }
+  return 'that does not exist in your basket'
+}
+
+
+
+
+
+
+
